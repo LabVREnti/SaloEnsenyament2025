fd2978501a79d0ff25f337ce8534f299ec73ce97
Lab <labvr@enti.cat>
2025-02-03 12:34:56 +0100

    VideoPlayer state machine done (fully working)

---
+++ Assets/Scripts/DurationBarHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DurationBarHandler : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+{
+    public VideoPlayerHandler videoPlayerHandlerRef;
+    private Slider durationBarRef;
+
+    private void Start()
+    {
+        durationBarRef = videoPlayerHandlerRef.durationBar;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        videoPlayerHandlerRef.State = VideoPlayerHandler.PlayerState.GRABBED;
+        durationBarRef.onValueChanged.AddListener(ForceTimePosition);        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        videoPlayerHandlerRef.State = VideoPlayerHandler.PlayerState.RELEASED;
+        durationBarRef.onValueChanged.RemoveListener(ForceTimePosition);
+    }
+
+    private void ForceTimePosition(float newVal)
+    {
+        videoPlayerHandlerRef.playerRef.time = newVal;
+    }
+}
